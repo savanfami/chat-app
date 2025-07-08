690f041d7c009e108e72afc87dead5483f582855
savanfami <savanfami1239@gmail.com>
2025-07-08 19:18:58 +0530

    code formatting

--- backend/src/socket/socket.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SocketStateService } from './socket.state.service';
-
-@Module({
-  providers: [SocketStateService],
-  exports: [SocketStateService], 
-})
-export class SocketModule {}
